c2189dc81ac6595b2b0c1058aed35817c0f9cb9e
Denis <1994osin@gmail.com>
2022-11-10 13:00:04 +0300

    Сделанное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 1
+        int theAmountInTheGlassJarBank = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total += theAmountInTheGlassJarBank;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println();
+
+        // Task 2
+        int amountToBank = 29000;
+        int totalMoneyInTheBank = 0;
+        for (int i = 1; i <= 12; i++) {
+            totalMoneyInTheBank = totalMoneyInTheBank + totalMoneyInTheBank / 100;
+            totalMoneyInTheBank += amountToBank;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalMoneyInTheBank + " рублей");
+        }
     }
 }
